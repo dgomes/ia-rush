01da46d64bf3836ee06c369291f831a7dcdf907f
Diogo Gomes <dgomes@ua.pt>
2022-11-19 00:44:43 +0000

    1a entrega

--- server.py
+++ server.py
@@ -50,7 +50,6 @@ class GameServer:
         if os.path.isfile(HIGHSCORE_FILE):
             with open(HIGHSCORE_FILE, "r") as infile:
                 self._highscores = json.load(infile)
-                print(self._highscores)
 
     def save_highscores(self, score: int):
         """Update highscores, storing to file."""
@@ -69,7 +68,6 @@ class GameServer:
             :MAX_HIGHSCORES
         ]
 
-        print(self._highscores)
 
         with open(HIGHSCORE_FILE, "w") as outfile:
             json.dump(self._highscores, outfile)
